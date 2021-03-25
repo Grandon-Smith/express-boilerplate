fdc94f47350bcf50fe870818d1af3b98aafa0e0b
Grandon Smith <grandon.smith@yahoo.com>
2021-03-25 13:33:02 -0400

    editing long overdue mistakes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const app = require('./app')
 
 const { PORT } = require('./config')
 
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
